3e5292cc63df46e6eb7048c7998bddfb0e4b21d9
ashok katwal <ashokkatwal9811@gmail.com>
2024-06-26 15:33:29 +0545

    sdfh

---
+++ backend/controller/user.controller.ts
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose'
+import express,{request,response} from 'express'
+
+const createUser = ()=>{
+    const {name,email,password}=req.body;
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose'
 const app=express();
 const port=4000
-app.listen(port,()=>{
-    console.log(`listening to the port${port}`)
-})
+
+mongoose.connect('mongodb://localhost:27017/ashok')
+    .then(()=>{
+        app.listen(port,()=>{
+            console.log(`listening to the port${port}`)
+        })
+    }).catch(error =>{
+        console.error("error",error)
+    })
