d5e46d85af4afd7cfbad62ff32fac703e06c7d0f
ashok katwal <ashokkatwal9811@gmail.com>
2024-07-04 10:56:44 +0545

    sdfj

--- naya/src/constant/enum.ts
+++ naya/src/constant/enum.ts
@@ -2,3 +2,16 @@ export enum Environment{
     DEVELOPMENT = 'DEVELOPMENT',
     PRODUCTION ='PRODUCTION'
 }
+export enum Role{
+    SUDO_ADMIN = 'SUDO_ADMIN',
+    SUPER_ADMIN = 'SUPER_ADMIN',
+    ADMIN ='ADMIN',
+    USER = 'USER'
+}
+export enum MediaType{
+    PROFILE='PROFILE'
+}
+export enum AdminAllowedFeature{
+    SETUP='SETUP',
+    MANAGE_ADMIN = 'MANAGE_ADMIN',
+}
